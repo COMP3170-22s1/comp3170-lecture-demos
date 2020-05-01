6b1782066b7e7de516c0f48cd10c08955b1f9ce3
Malcolm Ryan <malcolm.ryan@gmail.com>
2020-05-01 14:09:04 +1000

    Added a set of axes

---
+++ src/comp3170/demos/week9/sceneobjects/Axes.java
@@ -0,0 +1,57 @@
+package comp3170.demos.week9.sceneobjects;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLContext;
+
+import comp3170.SceneObject;
+import comp3170.SceneObjectOld;
+import comp3170.Shader;
+
+public class Axes extends SceneObject {
+
+	private float[] vertices = {
+			0,0,0,	// x
+			1,0,0,
+
+			0,0,0,	// y
+			0,1,0,
+
+			0,0,0,	// z
+			0,0,1,
+	};
+	private int vertexBuffer;
+
+	private float[] colours = {
+			1,0,0,	// x = red
+			1,0,0,
+
+			0,1,0,	// y = green
+			0,1,0,
+
+			0,0,1,	// z = blue
+			0,0,1,
+
+	}; 
+	private int colourBuffer;
+
+	public Axes(Shader shader) {
+		super(shader);
+		
+		this.vertexBuffer = shader.createBuffer(this.vertices);
+		this.colourBuffer = shader.createBuffer(this.colours);
+	}
+
+	@Override
+	protected void drawSelf(Shader shader) {
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+
+		shader.setUniform("u_mvpMatrix", this.mvpMatrix);		
+		shader.setAttribute("a_position", this.vertexBuffer);
+		shader.setAttribute("a_colour", this.colourBuffer);
+
+		gl.glDrawArrays(GL.GL_LINES, 0, this.vertices.length / 3);
+
+	}
+
+}
