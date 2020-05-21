45ca30acc93804e7777bcc25571ba8978ca35754
Malcolm Ryan <malcolm.ryan@gmail.com>
2020-05-21 21:27:21 +1000

    It works! (More or less)

--- src/comp3170/demos/week12/shaders/shadowFragment.glsl
+++ src/comp3170/demos/week12/shaders/shadowFragment.glsl
@@ -9,17 +9,20 @@ in vec4 v_position;		// WORLD
 
 layout(location = 0) out vec4 colour;
 
+const float BIAS = 0.0001;
+
 void main() {
 	// compare to the value from the shadow buffer
 	vec4 shadowPos = u_lightMatrix * v_position;
+	shadowPos = shadowPos / shadowPos.w; 
 	
 	// scale into [0,1]
 	shadowPos = (shadowPos + 1) / 2; 
 	vec4 minDist = texture(u_shadowBuffer, shadowPos.xy);
 	
-	if (shadowPos.z > minDist.z) {
+	if (shadowPos.z >= minDist.z + 0.00001) {
 		// in shadow
-		colour = shadowPos;
+		colour = vec4(0,0,0,1);
 	}
 	else {
 		// lit
