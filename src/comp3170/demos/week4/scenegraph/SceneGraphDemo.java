f8e208d0f0e718c3b1a54d4d3763ec710cbaf464
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-03-12 17:58:05 +1100

    New week4 demo

---
+++ src/comp3170/demos/week4/scenegraph/SceneGraphDemo.java
@@ -0,0 +1,165 @@
+
+package comp3170.demos.week4.scenegraph;
+
+import static com.jogamp.opengl.GL.GL_COLOR_BUFFER_BIT;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+import org.joml.Matrix3f;
+
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.Animator;
+
+import comp3170.GLException;
+import comp3170.InputManager;
+import comp3170.Shader;
+
+public class SceneGraphDemo extends JFrame implements GLEventListener {
+
+	public static final float TAU = (float) (2 * Math.PI);		// https://tauday.com/tau-manifesto
+	
+	private int width = 800;
+	private int height = 800;
+
+	private GLCanvas canvas;
+	private Shader shader;
+	
+	final private File DIRECTORY = new File("src/comp3170/demos/week4/scenegraph"); 
+	final private String VERTEX_SHADER = "vertex.glsl";
+	final private String FRAGMENT_SHADER = "fragment.glsl";
+
+	private Animator animator;
+	private long oldTime;
+	private InputManager input;
+
+	private Matrix3f viewMatrix;
+	private Matrix3f projectionMatrix;
+
+	public SceneGraphDemo() {
+		super("Week 4 scene graph demo");
+
+		// set up a GL canvas
+		GLProfile profile = GLProfile.get(GLProfile.GL4);		 
+		GLCapabilities capabilities = new GLCapabilities(profile);
+		this.canvas = new GLCanvas(capabilities);
+		this.canvas.addGLEventListener(this);
+		this.add(canvas);
+		
+		// set up the JFrame
+		
+		this.setSize(width,height);
+		this.setVisible(true);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		// set up Animator		
+		this.animator = new Animator(canvas);
+		this.animator.start();
+		this.oldTime = System.currentTimeMillis();		
+		
+		// set up Input manager
+		this.input = new InputManager();
+		input.addListener(this);
+		input.addListener(this.canvas);
+
+	}
+
+	private static final int NSQUARES = 100;
+	
+	@Override
+	public void init(GLAutoDrawable arg0) {
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+		
+		// set the background colour to black
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		
+		// Compile the shader
+		try {
+			File vertexShader = new File(DIRECTORY, VERTEX_SHADER);
+			File fragementShader = new File(DIRECTORY, FRAGMENT_SHADER);
+			this.shader = new Shader(vertexShader, fragementShader);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (GLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		// Set up the scene
+		
+	    
+	    // allocation view and projection matrices
+	    viewMatrix = new Matrix3f();
+	    projectionMatrix = new Matrix3f();
+	}
+
+	private static final float ROTATION_SPEED = TAU / 6;
+	private static final float CAMERA_ROTATION_SPEED = TAU / 6;
+	private static final float CAMERA_MOVEMENT_SPEED = 0.5f;
+	private static final float CAMERA_ZOOM_SPEED = 1.5f;
+	
+	private void update() {
+		long time = System.currentTimeMillis();
+		float deltaTime = (time - oldTime) / 1000f;
+		oldTime = time;
+		
+		input.clear();		
+	}
+	
+	@Override
+	public void display(GLAutoDrawable arg0) {
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+		
+		// update the scene
+		update();	
+
+        // clear the colour buffer
+		gl.glClear(GL_COLOR_BUFFER_BIT);		
+
+		// activate the shader
+		this.shader.enable();		
+		
+		
+		viewMatrix.identity();
+		projectionMatrix.identity();
+		
+		shader.setUniform("u_viewMatrix", viewMatrix);
+		shader.setUniform("u_projectionMatrix", projectionMatrix);
+		
+		
+		
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable d, int x, int y, int width, int height) {
+		this.width = width;
+		this.height = height;		
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) { 
+		new SceneGraphDemo();
+	}
+
+
+}
