69e83b37a1272ec6e87a541a98aa26757c0ef21a
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-03-26 21:04:59 +1100

    Fixed bug in Mesh.setColour

--- src/comp3170/demos/week5/Mesh.java
+++ src/comp3170/demos/week5/Mesh.java
@@ -72,8 +72,8 @@ abstract public class Mesh {
 
 	public void setColour(Color color) {		
 		colour.x = color.getRed() / 255f;
-		colour.y = color.getBlue() / 255f;
-		colour.z = color.getGreen() / 255f;
+		colour.y = color.getGreen() / 255f;
+		colour.z = color.getBlue() / 255f;
 	}
 
 	abstract public void draw();
