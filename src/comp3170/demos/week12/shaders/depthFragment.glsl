81bb542ce19ef52fd3530c63fd2447e0303dc1d7
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-04-27 20:59:19 +1000

    Deleted last year's demos

--- src/comp3170/demos/week12/shaders/depthFragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 410
-
-layout(location = 0) out vec4 colour;
-
-void main() {
-	float depth = gl_FragCoord.z;
-	colour = vec4(depth, depth, depth, 1);
-}
-
--- src/comp3170/demos/week12/shaders/depthVertex.glsl
+++
@@ -1,9 +0,0 @@
-#version 410
-
-in vec3 a_position;		// MODEL
-uniform mat4 u_mvpMatrix;	// MODEL -> NDC
-
-void main() {
-    gl_Position = u_mvpMatrix * vec4(a_position, 1);    
-}
-
