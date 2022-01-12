1fb820a3f8299ee32dfb20fcea52fbe4fe164ada
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2022-01-12 11:45:20 +1100

    Working on shadow buffer demo

--- src/comp3170/demos/week12/demos/ShadowBufferDemo.java
+++ src/comp3170/demos/week12/demos/ShadowBufferDemo.java
@@ -1,10 +1,12 @@
 
 package comp3170.demos.week12.demos;
+
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.util.List;
 
 import javax.swing.JFrame;
 
@@ -33,14 +35,14 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 
 	final private static float TAU = (float) (Math.PI * 2);
 	static final private String DEPTH_VERTEX_SHADER = "depthVertex.glsl";
-	static final private String DEPTH_FRAGMENT_SHADER = "depthFragment.glsl";	
+	static final private String DEPTH_FRAGMENT_SHADER = "depthFragment.glsl";
 	static final private String SIMPLE_VERTEX_SHADER = "simpleVertex.glsl";
-	static final private String SIMPLE_FRAGMENT_SHADER = "simpleFragment.glsl";	
-	
+	static final private String SIMPLE_FRAGMENT_SHADER = "simpleFragment.glsl";
+
 	// screen size in pixels
 	private int screenWidth = 1000;
 	private int screenHeight = 1000;
-	
+
 	private InputManager input;
 	private Animator animator;
 	private long oldTime;
@@ -48,7 +50,7 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 	private Camera camera;
 	private Cylinder cylinder;
 	private Plane plane;
-	
+
 	private int renderSize = 4096;
 	private int renderTexture;
 	private int frameBuffer;
@@ -57,36 +59,34 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 
 	private int depthTexture;
 
-
-	
 	public ShadowBufferDemo() {
 		super("Shadow buffer demo");
-		
-		GLProfile profile = GLProfile.get(GLProfile.GL4);		 
+
+		GLProfile profile = GLProfile.get(GLProfile.GL4);
 		GLCapabilities capabilities = new GLCapabilities(profile);
 		this.canvas = new GLCanvas(capabilities);
 		this.canvas.addGLEventListener(this);
 		this.add(canvas);
-		
-		// set up Animator		
+
+		// set up Animator
 		this.animator = new Animator(canvas);
 		this.animator.start();
 		this.oldTime = System.currentTimeMillis();
-				
+
 		// set up Input manager
 		this.input = new InputManager(canvas);
-		
-		// set up the JFrame		
+
+		// set up the JFrame
 		// make it twice as wide as the view width
-		
+
 		this.setSize(screenWidth, screenHeight);
 		this.setVisible(true);
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
-		});	
-		
+		});
+
 	}
 
 	@Override
@@ -95,9 +95,9 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 	 */
 	public void init(GLAutoDrawable drawable) {
 		GL4 gl = (GL4) GLContext.getCurrentGL();
-		
+
 		// enable depth testing
-		
+
 		gl.glEnable(GL.GL_DEPTH_TEST);
 		gl.glClearDepth(1f);
 		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -106,19 +106,19 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 		Shader depthShader = ShaderLibrary.compileShader(DEPTH_VERTEX_SHADER, DEPTH_FRAGMENT_SHADER);
 
 		this.cylinder = new Cylinder(depthShader, Color.RED);
-		cylinder.setScale(1,2,1);
+		cylinder.setScale(1, 2, 1);
 		this.plane = new Plane(depthShader, Color.WHITE);
-		plane.setScale(10,1,10);
-		
-		float aspect = (float)screenWidth / screenHeight;
+		plane.setScale(10, 1, 10);
+
+		float aspect = (float) screenWidth / screenHeight;
 		this.camera = new OrthographicCamera(CAMERA_HEIGHT * aspect, CAMERA_HEIGHT, CAMERA_NEAR, CAMERA_FAR);
 //		this.camera = new PerspectiveCamera(CAMERA_FOVY, aspect, CAMERA_NEAR, CAMERA_FAR);
 		camera.setDistance(CAMERA_DISTANCE);
-		camera.setTarget(0,1,0);	
-				
+		camera.setTarget(0, 1, 0);
+
 		this.renderTexture = Shader.createRenderTexture(renderSize, renderSize);
 		this.depthTexture = Shader.createDepthTexture(renderSize, renderSize);
-		
+
 		try {
 			this.frameBuffer = Shader.createFrameBuffer(renderTexture, depthTexture);
 		} catch (GLException e) {
@@ -131,22 +131,21 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 	private static final float CAMERA_HEIGHT = 30;
 	private static final float CAMERA_FOVY = TAU / 8;
 	private static final float CAMERA_NEAR = 0.1f;
-	private static final float CAMERA_FAR = 50.0f;	
-	
+	private static final float CAMERA_FAR = 50.0f;
+
 	public void update() {
 		long time = System.currentTimeMillis();
 		float dt = (time - oldTime) / 1000.0f;
 		oldTime = time;
-		
+
 		if (input.wasKeyPressed(KeyEvent.VK_SPACE)) {
-			this.useEffect = !this.useEffect ;
+			this.useEffect = !this.useEffect;
 		}
-		
+
 		camera.update(input, dt);
 		input.clear();
 	}
-	
-	
+
 	@Override
 	/**
 	 * Called when the canvas is redrawn
@@ -157,9 +156,9 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 		update();
 
 		gl.glBindFramebuffer(GL.GL_FRAMEBUFFER, 0);
-		gl.glViewport(0, 0, this.screenWidth, this.screenHeight);			
-		gl.glClear(GL.GL_COLOR_BUFFER_BIT);		
-		gl.glClear(GL.GL_DEPTH_BUFFER_BIT);		
+		gl.glViewport(0, 0, this.screenWidth, this.screenHeight);
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
 		this.cylinder.draw(camera);
 		this.plane.draw(camera);
 	}
@@ -170,14 +169,14 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 	 */
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
 		GL4 gl = (GL4) GLContext.getCurrentGL();
-		
+
 		this.screenWidth = width;
-		this.screenHeight = height;		
+		this.screenHeight = height;
 	}
 
 	@Override
 	/**
-	 * Called when we dispose of the canvas 
+	 * Called when we dispose of the canvas
 	 */
 	public void dispose(GLAutoDrawable drawable) {
 
@@ -186,6 +185,5 @@ public class ShadowBufferDemo extends JFrame implements GLEventListener {
 	public static void main(String[] args) throws IOException, GLException {
 		new ShadowBufferDemo();
 	}
-
 
 }
