10d8b84d90343377dfdf351f5e07263974efce2a
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2022-03-06 15:37:41 +1100

    Added model and view matrices

--- src/comp3170/demos/week3/demos/vertex.glsl
+++ src/comp3170/demos/week3/demos/vertex.glsl
@@ -2,7 +2,11 @@
 
 in vec2 a_position;	// vertex position as a 2D vector in MODEL
 
+uniform mat4 u_modelMatrix;
+uniform mat4 u_viewMatrix;
+
 void main() {
-    gl_Position = vec4(a_position,0,1);
+	vec4 position = vec4(a_position,0,1);
+    gl_Position = u_viewMatrix * u_modelMatrix * position;
 }
 
